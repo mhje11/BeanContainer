37248b9b1ae91aaea675ad269057c2ef26dfe212
mhje11 <mhje1122@gmail.com>
2024-07-29 10:05:56 +0900

    add: add dev branch

--- src/main/java/com/beancontainer/domain/cafe/Cafe.java
+++ src/main/java/com/beancontainer/domain/cafe/Cafe.java
@@ -1,4 +1,13 @@
 package com.beancontainer.domain.cafe;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cafe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
