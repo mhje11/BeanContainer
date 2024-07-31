fdda99aaac743b6718bf69136584f9af239cf35d
김지은 <wldmsdl2395@naver.com>
2024-08-01 01:21:15 +0900

    Feat: Create Post

--- src/main/java/com/beancontainer/domain/post/service/PostService.java
+++ src/main/java/com/beancontainer/domain/post/service/PostService.java
@@ -1,11 +1,43 @@
 package com.beancontainer.domain.post.service;
 
+import com.beancontainer.domain.post.dto.PostRequestDto;
+import com.beancontainer.domain.post.dto.PostResponseDto;
+import com.beancontainer.domain.post.entity.Post;
 import com.beancontainer.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
     private final PostRepository postRepository;
+
+    @Transactional
+    public PostResponseDto createPost(PostRequestDto postRequestDto) {
+//        Member testMember = new Member(1L, "test", "nick", "test", "1234", null, null);
+
+        Post post = new Post(
+                postRequestDto.getUsername(),
+                postRequestDto.getTitle(),
+                postRequestDto.getContent(),
+                postRequestDto.getUuid()
+        );
+
+        Post savedPost = postRepository.save(post);
+        log.info("Saved post: {}", savedPost.getUsername());
+
+        return new PostResponseDto(
+                savedPost.getId(),
+                savedPost.getUsername(),
+                savedPost.getTitle(),
+                savedPost.getContent(),
+//                savedPost.getViews(),
+//                savedPost.getCreatedAt(),
+//                savedPost.getUpdatedAt(),
+                savedPost.getUuid()
+        );
+    }
 }
