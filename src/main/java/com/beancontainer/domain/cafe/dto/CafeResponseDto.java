2271a0dd0a2522be53c51af09238c5b54e958065
mhje11 <mhje1122@gmail.com>
2024-07-31 14:44:03 +0900

    Test: add cafe save, get test

--- src/main/java/com/beancontainer/domain/cafe/dto/CafeResponseDto.java
+++ src/main/java/com/beancontainer/domain/cafe/dto/CafeResponseDto.java
@@ -5,13 +5,19 @@ import lombok.Getter;
 
 @Getter
 public class CafeResponseDto {
+    private Long id;
     private String name;
     private String address;
     private String district;
+    private Double longitude;
+    private Double latitude;
 
     public CafeResponseDto(Cafe cafe) {
+        this.id = cafe.getId();
         this.name = cafe.getName();
         this.address = cafe.getAddress();
         this.district = cafe.getDistrict();
+        this.longitude = cafe.getLongitude();
+        this.latitude = cafe.getLatitude();
     }
 }
