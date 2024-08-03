6727a88cea1137347714a054c6a511e4a25de370
김지은 <wldmsdl2395@naver.com>
2024-08-04 03:37:57 +0900

    Test: Post List

--- src/main/java/com/beancontainer/domain/post/dto/PostListResponseDto.java
+++ src/main/java/com/beancontainer/domain/post/dto/PostListResponseDto.java
@@ -9,13 +9,13 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostResponseDto {
+public class PostListResponseDto {
     private Long id;
-    private String username;
     private String title;
-    private String content;
-    private int views;
+    private String nickname;
+    // 댓글 수
+    // 좋아요 수
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-//    private String uuid;
+    private int views;
 }
