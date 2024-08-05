59bddce70f1fbd95b0f2cc24fccf90530c6e005b
chahyeon min <chahyeonmin91@gmail.com>
2024-08-05 18:28:38 +0900

    Feat: Chat Message

--- src/main/java/com/beancontainer/domain/chatroom/dto/ChatRoomDto.java
+++ src/main/java/com/beancontainer/domain/chatroom/dto/ChatRoomDto.java
@@ -1,14 +1,25 @@
 package com.beancontainer.domain.chatroom.dto;
 
+
+import com.beancontainer.domain.chatroom.entity.ChatRoom;
 import lombok.Getter;
 import lombok.Setter;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ChatRoomDto {
     private Long id;
     private String title;
-    private int maxParticipant;
     private String ownerName;
-    private String district;
+    private LocalDateTime createdAt;
+    private int maxParticipant;
+
+    public ChatRoomDto(ChatRoom chatRoom) {
+        this.id = chatRoom.getId();
+        this.title = chatRoom.getTitle();
+        this.ownerName = chatRoom.getOwnerName();
+        this.createdAt = chatRoom.getCreatedAt();
+        this.maxParticipant = chatRoom.getMaxParticipant();
+    }
 }
--- src/main/java/com/beancontainer/domain/message/controller/MessageController.java
+++ src/main/java/com/beancontainer/domain/message/controller/MessageController.java
@@ -1,31 +1,22 @@
 package com.beancontainer.domain.message.controller;
 
 
+
 import com.beancontainer.domain.message.entity.Message;
 import com.beancontainer.domain.message.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/messages")
+@RequiredArgsConstructor
 public class MessageController {
-
     private final MessageService messageService;
 
-    @Autowired
-    public MessageController(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
-    @GetMapping("/chat/{chatRoomId}/messages")
-    public List<Message> getMessages(@PathVariable Long chatRoomId, @RequestParam int page, @RequestParam int size) {
-        return messageService.getMessages(chatRoomId, page, size);
-    }
-
-    @PostMapping("/chat/sendMessage")
-    public Message sendMessage(@RequestBody Message message) {
-        return messageService.saveMessage(message);
+    @GetMapping("/chatroom/{roomId}")
+    public List<Message> getMessages(@PathVariable Long roomId, @RequestParam int page, @RequestParam int size) {
+        return messageService.getMessages(roomId, page, size);
     }
 }
