1559f69568073591831e5041fde56e942bb3195c
mhje11 <mhje1122@gmail.com>
2024-08-05 20:38:44 +0900

    Feat: add Create MyMap

--- src/main/java/com/beancontainer/domain/map/Controller/MapRestController.java
+++ src/main/java/com/beancontainer/domain/map/Controller/MapRestController.java
@@ -5,8 +5,8 @@ import com.beancontainer.domain.map.dto.MapDetailResponseDto;
 import com.beancontainer.domain.map.dto.MapListResponseDto;
 import com.beancontainer.domain.map.dto.MapUpdateDto;
 import com.beancontainer.domain.map.service.MapService;
-import com.beancontainer.domain.mapcafe.repository.MapCafeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,12 +16,15 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MapRestController {
     private final MapService mapService;
 
     @PostMapping("/api/mymap")
     public ResponseEntity<String> createMap(@RequestBody MapCreateDto mapCreateDto, @AuthenticationPrincipal UserDetails userDetails) {
         mapCreateDto.setUsername(userDetails.getUsername());
+        log.info("Received Map Data: {}", mapCreateDto.getKakaoIds()); // 로그 추가
+
         Long mapId = mapService.createMap(mapCreateDto);
         return ResponseEntity.ok("지도 생성 성공 : ID : " + mapId);
     }
