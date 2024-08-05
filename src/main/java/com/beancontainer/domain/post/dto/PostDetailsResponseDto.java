79bbfca96a6906fbbecaa478d76cd021000cd0bd
김지은 <wldmsdl2395@naver.com>
2024-08-05 13:33:49 +0900

    Feat: Post Update

--- src/main/java/com/beancontainer/domain/post/dto/PostDetailsResponseDto.java
+++ src/main/java/com/beancontainer/domain/post/dto/PostDetailsResponseDto.java
@@ -1,5 +1,6 @@
 package com.beancontainer.domain.post.dto;
 
+import com.beancontainer.domain.post.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,13 @@ public class PostDetailsResponseDto {
     private int views;
     private String content;
 
+    public PostDetailsResponseDto(Post updatedPost) {
+        this.id = updatedPost.getId();
+        this.title = updatedPost.getTitle();
+        this.nickname = updatedPost.getMember().getNickname();
+        this.createdAt = updatedPost.getCreatedAt();
+        this.updatedAt = updatedPost.getUpdatedAt();
+        this.views = updatedPost.getViews();
+        this.content = updatedPost.getContent();
+    }
 }
