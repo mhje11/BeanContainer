607e14afc57ad679089c7475cc7a74754321956f
chahyeon min <chahyeonmin91@gmail.com>
2024-08-07 02:16:17 +0900

    Feat: Chat Stomp

--- src/main/java/com/beancontainer/domain/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/beancontainer/domain/chatroom/repository/ChatRoomRepository.java
@@ -1,11 +1,34 @@
 package com.beancontainer.domain.chatroom.repository;
 
+import com.beancontainer.domain.chatroom.chatDto.ChatRoom;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
-import com.beancontainer.domain.chatroom.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
-import java.util.List;
+@Repository
+public class ChatRoomRepository {
+    private Map<String, ChatRoom> chatRoomMap;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    List<ChatRoom> findByOwnerName(String ownerName);
+    @PostConstruct
+    private void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findALlRoom() {
+        //채팅방 생성순서 최근 순으로 반환
+        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = new ChatRoom().create(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
 }
