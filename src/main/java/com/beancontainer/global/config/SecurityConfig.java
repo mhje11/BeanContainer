8c231b71657df7a884f14b603a131d943ce7c30f
minjin00 <dnfenskaao@naver.com>
2024-08-07 09:53:23 +0900

    Fix: mypage 인가 문제 해결

--- src/main/java/com/beancontainer/global/config/SecurityConfig.java
+++ src/main/java/com/beancontainer/global/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/login", "/api/auth/signup").permitAll()
                         .requestMatchers("/post/create").permitAll()
                         .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/mypage/**").authenticated() // 인증된 사용자만 접근 가능
+                        .requestMatchers("/mypage/{userId}").authenticated() // 인증된 사용자만 접근 가능
                         .requestMatchers("/admin").hasRole("ADMIN") // ROLE 이 ADMIN 인 사람만 접근 가능
                         .requestMatchers("/review/{kakaoId}").permitAll()
                         .anyRequest().authenticated() //그 외 모든 요청은 인증 필요
