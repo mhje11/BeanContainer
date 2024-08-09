a4342b542d671c5e8b5d6618a93eee094b1f8c03
chahyeon min <chahyeonmin91@gmail.com>
2024-08-10 00:09:16 +0900

    Feat: Chat Only Redis

--- src/main/java/com/beancontainer/domain/chatroom/controller/WebSocketController.java
+++ src/main/java/com/beancontainer/domain/chatroom/controller/WebSocketController.java
@@ -1,13 +1,13 @@
 package com.beancontainer.domain.chatroom.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
-    @GetMapping({"","/index"})
-    public String index() {
-        return "redirect:/chat/room";
+public class WebSocketController {
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
     }
 }
