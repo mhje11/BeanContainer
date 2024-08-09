20af2b82d37f8ba4d4f724d2801e87d92433a081
mhje11 <mhje1122@gmail.com>
2024-08-09 14:09:44 +0900

    Refactor: remove cafeCategory entity

--- src/main/java/com/beancontainer/domain/category/entity/Category.java
+++ src/main/java/com/beancontainer/domain/category/entity/Category.java
@@ -1,6 +1,5 @@
 package com.beancontainer.domain.category.entity;
 
-import com.beancontainer.domain.cafecategory.CafeCategory;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,9 +16,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CafeCategory> cafeCategories = new HashSet<>();
 
     public Category(String name) {
         this.name = name;
