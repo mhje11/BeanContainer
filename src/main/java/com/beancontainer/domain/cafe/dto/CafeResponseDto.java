ac2d8cd47359463def26095e75ca7a4a391da809
mhje11 <mhje1122@gmail.com>
2024-08-09 17:26:36 +0900

    Feat: add MyMap Modify

--- src/main/java/com/beancontainer/domain/cafe/dto/CafeResponseDto.java
+++ src/main/java/com/beancontainer/domain/cafe/dto/CafeResponseDto.java
@@ -3,17 +3,19 @@ package com.beancontainer.domain.cafe.dto;
 import com.beancontainer.domain.cafe.entity.Cafe;
 import lombok.Getter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 public class CafeResponseDto {
     private Long id;
+    private String kakaoId;
     private String name;
     private String address;
     private String district;
     private Double longitude;
     private Double latitude;
-    private Set<String> topCategories;
+    private Set<String> topCategories = new HashSet<>();
     private Double averageScore;
 
 
@@ -25,6 +27,7 @@ public class CafeResponseDto {
         this.longitude = cafe.getLongitude();
         this.latitude = cafe.getLatitude();
         this.topCategories = cafe.getTopCategories();
+        this.kakaoId = cafe.getKakaoId();
         this.averageScore = averageScore != null ? averageScore : 0.0;
     }
 }
