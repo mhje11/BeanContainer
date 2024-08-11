953a2770f44521833d527e35f784ee6b8ea69836
Chahyeon Min <chahyeonmin91@gmail.com>
2024-08-12 00:25:36 +0900

    redis&websocket

---
+++ src/main/java/com/beancontainer/domain/chatroom/controller/ChatRoomController.java
@@ -0,0 +1,35 @@
+package com.beancontainer.domain.chatroom.controller;
+
+import com.beancontainer.domain.chatroom.dto.ChatRoomDto;
+import com.beancontainer.domain.chatroom.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("chat")
+@RequiredArgsConstructor
+
+public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+
+
+    @GetMapping("/rooms")
+    public String getRoomsPage() {
+        return "chatroomlist";  // 채팅방 리스트 페이지
+    }
+
+    @GetMapping("/room/enter/{roomId}")
+    public String getChatRoomPage(@PathVariable String roomId, Model model) {
+        model.addAttribute("roomId", roomId);
+        return "chat";  // 특정 채팅방 입장 후의 채팅 페이지
+    }
+
+
+    @PostMapping("room")
+    public ChatRoomDto createRoom(@RequestParam String name) {
+        return chatRoomService.createChatRoom(name);
+    }
+}
