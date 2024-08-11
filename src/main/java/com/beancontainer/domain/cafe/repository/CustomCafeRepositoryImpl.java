69f1c806271d2f035f4dd6c81b1548330b916c9d
mhje11 <mhje1122@gmail.com>
2024-08-11 15:10:44 +0900

    Feat: add Search by Category

--- src/main/java/com/beancontainer/domain/cafe/repository/CustomCafeRepositoryImpl.java
+++ src/main/java/com/beancontainer/domain/cafe/repository/CustomCafeRepositoryImpl.java
@@ -2,10 +2,10 @@ package com.beancontainer.domain.cafe.repository;
 
 import com.beancontainer.domain.cafe.entity.Cafe;
 import com.beancontainer.domain.cafe.entity.QCafe;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,9 +21,15 @@ public class CustomCafeRepositoryImpl implements CustomCafeRepository{
 
     @Override
     public List<Cafe> findByCategories(Set<String> categories) {
+        queryFactory = new JPAQueryFactory(em);
         QCafe cafe = QCafe.cafe;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 각 카테고리에 대해 조건을 추가
+        categories.forEach(category -> builder.and(cafe.topCategories.contains(category)));
+
         return queryFactory.selectFrom(cafe)
-                .where(cafe.topCategories.any().in(categories))
+                .where(builder)
                 .fetch();
     }
 }
