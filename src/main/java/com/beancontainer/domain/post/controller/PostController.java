3d653e211678ade46f6364995cf227b5f49a8d7c
김지은 <wldmsdl2395@naver.com>
2024-08-12 15:29:25 +0900

    Rename: html about Post

--- src/main/java/com/beancontainer/domain/post/controller/PostController.java
+++ src/main/java/com/beancontainer/domain/post/controller/PostController.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class PostController {
     // 게시글 전체 조회
-    @GetMapping("/post/postList")
+    @GetMapping("/post/post-list")
     public String postList() {
-        return "post/postList";
+        return "/post/post-list";
     }
 
     // 게시글 작성 폼
     @GetMapping("/post/create")
     public String postCreateForm() {
-        return "post/postCreate";
+        return "post/post-create";
     }
 
     // 게시글 상세보기 폼
     @GetMapping("/postList/{postId}")
     public String postDetailsForm(@PathVariable Long postId) {
-        return "post/postDetails";
+        return "post/post-details";
     }
 
     // 게시글 수정 폼
     @GetMapping("/postList/update/{postId}")
     public String postUpdateForm(@PathVariable Long postId) {
-        return "post/postUpdate";
+        return "post/post-update";
     }
 }
