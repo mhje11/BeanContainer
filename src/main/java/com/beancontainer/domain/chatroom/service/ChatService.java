340c545f3c6126402ffbb89e499ed02419aabb04
chahyeon min <chahyeonmin91@gmail.com>
2024-08-12 17:02:34 +0900

    websocket..

---
+++ src/main/java/com/beancontainer/domain/chatroom/service/ChatService.java
@@ -0,0 +1,42 @@
+package com.beancontainer.domain.chatroom.service;
+
+import com.beancontainer.domain.chatroom.entity.ChatRoom;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+
+    private final ObjectMapper objectMapper;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    public ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        ChatRoom chatRoom = ChatRoom.builder()
+                .roomId(randomId)
+                .name(name)
+                .build();
+        chatRooms.put(randomId, chatRoom);
+        return chatRoom;
+    }
+}
---
+++ src/main/java/com/beancontainer/global/config/WebSockConfig.java
@@ -0,0 +1,22 @@
+package com.beancontainer.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket   //이게 websocket 서버로서 동작하겠다는 어노테이션
+public class WebSockConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+        // handler 등록,   js에서 new Websocket할 때 경로 지정
+        //다른 url에서도 접속할 수있게(CORS방지)
+    }
+}
