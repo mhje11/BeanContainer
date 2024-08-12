617f832d266c94d3577a8a5e46c13ee81305215f
chahyeon min <chahyeonmin91@gmail.com>
2024-08-12 18:01:20 +0900

    wesocket2

--- src/main/java/com/beancontainer/domain/chatroom/controller/ChatController.java
+++ src/main/java/com/beancontainer/domain/chatroom/controller/ChatController.java
@@ -1,43 +1,21 @@
 package com.beancontainer.domain.chatroom.controller;
 
-import com.beancontainer.domain.chatroom.entity.ChatRoom;
-import com.beancontainer.domain.chatroom.service.ChatService;
+import com.beancontainer.domain.chatroom.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
-@Controller
 @RequiredArgsConstructor
+@Controller
 public class ChatController {
-    private final ChatService chatService;
 
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @RequestMapping("/chat/chatList")
-    public String chatList(Model model){
-        List<ChatRoom> roomList = chatService.findAllRoom();
-        model.addAttribute("roomList",roomList);
-        return "chat/chatList";
-    }
-
-
-    @PostMapping("/chat/createRoom")
-    public String createRoom(Model model, @RequestParam String name, String username) {
-        ChatRoom room = chatService.createRoom(name);
-        model.addAttribute("room",room);
-        model.addAttribute("username",username);
-        return "chat/chatRoom";
-    }
-
-    @GetMapping("/chat/chatRoom")
-    public String chatRoom(Model model, @RequestParam String roomId){
-        ChatRoom room = chatService.findRoomById(roomId);
-        model.addAttribute("room",room);
-        return "chat/chatRoom";
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
--- src/main/java/com/beancontainer/global/config/WebSockConfig.java
+++ src/main/java/com/beancontainer/global/config/WebSockConfig.java
@@ -2,21 +2,22 @@ package com.beancontainer.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSocket   //이게 websocket 서버로서 동작하겠다는 어노테이션
-public class WebSockConfig implements WebSocketConfigurer {
-    private final WebSocketHandler webSocketHandler;
+@EnableWebSocketMessageBroker   //이게 websocket 서버로서 동작하겠다는 어노테이션
+public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
-        // handler 등록,   js에서 new Websocket할 때 경로 지정
-        //다른 url에서도 접속할 수있게(CORS방지)
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 }
