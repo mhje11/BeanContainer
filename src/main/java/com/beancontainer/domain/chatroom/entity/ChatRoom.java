be33f157d43097a0b15e6a0ae9dd234fca8f2e91
chahyeon min <chahyeonmin91@gmail.com>
2024-08-12 17:02:34 +0900

    websocket..

--- src/main/java/com/beancontainer/domain/chatroom/entity/ChatRoom.java
+++ src/main/java/com/beancontainer/domain/chatroom/entity/ChatRoom.java
@@ -1,22 +1,21 @@
 package com.beancontainer.domain.chatroom.entity;
 
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.web.socket.WebSocketSession;
 
-import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
-@Setter
 public class ChatRoom {
-    private String roomId; // 채팅방의 고유 ID
-    private String name;   // 채팅방 이름
-
-    // 새로운 채팅방을 생성하는 정적 메서드
-    public static ChatRoom create(String name) {
-        ChatRoom chatRoom = new ChatRoom();
-        chatRoom.roomId = UUID.randomUUID().toString(); // 고유한 ID 생성
-        chatRoom.name = name;
-        return chatRoom;
+    private String roomId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+    @Builder
+    public ChatRoom(String roomId, String name) {
+        this.roomId = roomId;
+        this.name = name;
     }
 }
