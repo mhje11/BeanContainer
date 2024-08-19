517ad930266078322a70f984530a440dcc653c9d
mhje11 <mhje1122@gmail.com>
2024-08-19 15:17:43 +0900

    Fix: fix review delete logic

--- src/main/java/com/beancontainer/domain/review/controller/ReviewRestController.java
+++ src/main/java/com/beancontainer/domain/review/controller/ReviewRestController.java
@@ -8,8 +8,10 @@ import com.beancontainer.domain.review.dto.ReviewUpdateDto;
 import com.beancontainer.domain.review.entity.Review;
 import com.beancontainer.domain.review.repository.ReviewRepository;
 import com.beancontainer.domain.review.service.ReviewService;
+import com.beancontainer.global.service.CustomUserDetails;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,6 +23,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewRestController {
 
     private final ReviewService reviewService;
@@ -35,7 +38,7 @@ public class ReviewRestController {
         Member member = memberRepository.findByUserId(userDetails.getUsername()).orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
         reviewService.createReview(reviewCreateDto, userDetails.getUsername());
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("지도 생성 완료");
+        return ResponseEntity.ok("리뷰 등록 완료");
     }
 
     @GetMapping("/api/reviewlist/{cafeId}")
@@ -49,24 +52,26 @@ public class ReviewRestController {
         Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new EntityNotFoundException("해당 리뷰를 찾을 수 없습니다."));
         if (userDetails == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 후 이용 가능합니다.");
-        } if (userDetails.getUsername().equals(review.getMember().getUserId())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("권한이 없습니다.");
+        } if (!userDetails.getUsername().equals(review.getMember().getUserId())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없습니다.");
         }
         reviewService.updateReview(reviewId, reviewUpdateDto);
-        return new ResponseEntity<>("리뷰가 수정되었습니다.", HttpStatus.OK);
+        return ResponseEntity.ok("리뷰 수정 완료");
     }
 
     @DeleteMapping("/api/review/delete/{reviewId}")
-    public ResponseEntity<String> deleteReview(@PathVariable("reviewId")Long reviewId, @AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<String> deleteReview(@PathVariable("reviewId")Long reviewId, @AuthenticationPrincipal CustomUserDetails userDetails) {
         Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new EntityNotFoundException("해당 리뷰를 찾을 수 없습니다."));
         if (userDetails == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 후 이용 가능합니다.");
-        } if (userDetails.getUsername().equals(review.getMember().getUserId())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("권한이 없습니다.");
+        } if (!userDetails.getUsername().equals(review.getMember().getUserId())) {
+            log.info("userDetails {}", userDetails.getUsername());
+            log.info("review {}", review.getMember().getUserId());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없습니다.");
         }
         reviewService.deleteReview(reviewId);
 
-        return new ResponseEntity<>("리뷰가 삭제되었습니다.", HttpStatus.OK);
+        return ResponseEntity.ok("리뷰 삭제 완료");
     }
 
 }
