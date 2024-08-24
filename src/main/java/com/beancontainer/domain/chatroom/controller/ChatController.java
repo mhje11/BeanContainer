01f1ab5a15afb3f2518013af111c9ceb1f98f8ad
chahyeon min <chahyeonmin91@gmail.com>
2024-08-25 08:10:43 +0900

    Feat: Web Socket

--- src/main/java/com/beancontainer/domain/chatroom/controller/ChatController.java
+++ src/main/java/com/beancontainer/domain/chatroom/controller/ChatController.java
@@ -1,41 +1,21 @@
 package com.beancontainer.domain.chatroom.controller;
 
-import com.beancontainer.domain.chatroom.dto.ChatMessageDTO;
-import com.beancontainer.domain.chatroom.service.ChatRoomService;
-import com.beancontainer.domain.chatroom.service.ChatService;
-import com.beancontainer.domain.member.entity.Member;
-import com.beancontainer.domain.member.repository.MemberRepository;
-import com.beancontainer.global.service.CustomUserDetails;
+import com.beancontainer.domain.chatroom.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
-@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    private final ChatRoomService chatRoomService;
-    private final ChatService chatService;
-    private final MemberRepository memberRepository;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    /**
-     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
-     */
     @MessageMapping("/chat/message")
-    public void message(ChatMessageDTO messageDTO, @AuthenticationPrincipal CustomUserDetails userDetails) {
-        Member member = memberRepository.findByUserId(userDetails.getUserId()).orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
-        String nickname = member.getNickname();
-        // 로그인 회원 정보로 대화명 설정
-        messageDTO.setSender(nickname);
-
-        // 채팅방 인원수 세팅
- //       messageDTO.setUserCount(chatRoomRepository.getUserCount(messageDTO.getRoomId()));
-
-        // Websocket에 발행된 메시지를 redis로 발행(publish)
-        chatService.sendChatMessage(messageDTO);
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
---
+++ src/main/java/com/beancontainer/domain/chatroom/dto/ChatRoom.java
@@ -0,0 +1,20 @@
+package com.beancontainer.domain.chatroom.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ChatRoom {
+    private String roomId;
+    private String name;
+
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
+    }
+}
