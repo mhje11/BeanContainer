c4030e1888f89e4c7c6548a50fa07a220e7fe3bb
chahyeon min <chahyeonmin91@gmail.com>
2024-08-27 16:05:45 +0900

    Fix:Chat Room Unique

--- src/main/java/com/beancontainer/domain/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/beancontainer/domain/chatroom/repository/ChatRoomRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    // 채팅방 이름으로 존재 여부를 확인하는 메서드 정의
+    boolean existsByName(String name);
 }
