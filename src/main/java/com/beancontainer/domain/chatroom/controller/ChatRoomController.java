d93402a5dce69a4ab66e3e0b027910642aee723d
chahyeon min <chahyeonmin91@gmail.com>
2024-08-30 00:24:50 +0900

    Fix: Capacity

--- src/main/java/com/beancontainer/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/beancontainer/domain/chatroom/controller/ChatRoomController.java
@@ -1,6 +1,8 @@
 package com.beancontainer.domain.chatroom.controller;
 
 import com.beancontainer.domain.chatroom.dto.ChatRoomDto;
+import com.beancontainer.domain.chatroom.entity.ChatRoom;
+import com.beancontainer.domain.chatroom.repository.ChatRoomRepository;
 import com.beancontainer.domain.chatroom.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.core.retry.RetryUtils;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,6 +23,7 @@ import java.util.UUID;
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
+    private final ChatRoomRepository chatRoomRepository;
 
     @GetMapping("/room")
     public String rooms(Model model) {
@@ -40,8 +44,15 @@ public class ChatRoomController {
 
     @GetMapping("/room/enter/{roomId}")
     public String roomDetail(Model model, @PathVariable Long roomId, @AuthenticationPrincipal UserDetails userDetails) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new RuntimeException("찾을수없는룸"));
+        if (chatRoom.getCurrentUserCount() >= chatRoom.getCapacity()) {
+            return "/chat/room";
+
+        }
         model.addAttribute("roomId", roomId);
         model.addAttribute("userId", userDetails.getUsername());
+        chatRoom.incrementUserCount();
+        chatRoomRepository.save(chatRoom);
         return "/chat/roomdetail";
     }
 
@@ -50,4 +61,14 @@ public class ChatRoomController {
     public ChatRoomDto roomInfo(@PathVariable Long roomId) {
         return chatRoomService.findRoomById(roomId);
     }
+
+    @PutMapping("/room/quit")
+    @ResponseBody
+    public void exitRoom(@ModelAttribute Long roomId) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new RuntimeException("방못찾음"));
+        chatRoom.decrementUserCount();
+        chatRoomRepository.save(chatRoom);
+
+    }
 }
+
---
+++ src/test/java/com/example/beancontainer/BeanContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beancontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
