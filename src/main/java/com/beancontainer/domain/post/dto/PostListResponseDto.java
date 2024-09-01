5150eb09ad179fb800009c0be56b8b48a4013b61
김지은 <wldmsdl2395@naver.com>
2024-09-02 06:52:21 +0900

    Design: add font about Post & change post-list

--- src/main/java/com/beancontainer/domain/post/dto/PostListResponseDto.java
+++ src/main/java/com/beancontainer/domain/post/dto/PostListResponseDto.java
@@ -13,6 +13,7 @@ public class PostListResponseDto {
     private Long id;
     private String title;
     private String nickname;
+    private String profileImageUrl;
     private int commentCount;   // 댓글 수
     private int likeCount;  // 좋아요 수
     private LocalDateTime createdAt;
