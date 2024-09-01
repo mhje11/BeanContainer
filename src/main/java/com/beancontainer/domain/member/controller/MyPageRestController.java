f26d3d56e7bcd2ad7d1b292a208915c5b1d24249
minjin00 <dnfenskaao@naver.com>
2024-09-01 18:32:50 +0900

    Fix: 회원탈퇴 즉시 쿠키 삭제

--- src/main/java/com/beancontainer/domain/member/controller/MyPageRestController.java
+++ src/main/java/com/beancontainer/domain/member/controller/MyPageRestController.java
@@ -2,6 +2,8 @@ package com.beancontainer.domain.member.controller;
 
 import com.beancontainer.domain.member.service.MemberService;
 import com.beancontainer.domain.member.profileimage.service.ProfileImageService;
+import com.beancontainer.global.auth.service.CookieService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,6 +23,7 @@ import java.util.Map;
 public class MyPageRestController {
     private final MemberService memberService;
     private final ProfileImageService profileImageService;
+    private final CookieService cookieService;
 
 
     @PostMapping("/{userId}/nickname")
@@ -61,13 +64,16 @@ public class MyPageRestController {
 
     @DeleteMapping("/{userId}/account")
     public ResponseEntity<String> deleteAccount(@PathVariable String userId,
-                                                Principal principal) {
+                                                Principal principal, HttpServletResponse response) {
         // 현재 로그인한 사용자와 userId가 일치하는지 확인
         if (!userId.equals(principal.getName())) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
         memberService.cancelAccount(userId);
+        // 관련된 모든 쿠키 삭제
+        cookieService.deleteCookie(response, "accessToken");
+        cookieService.deleteCookie(response, "refreshToken");
         return ResponseEntity.ok().body("계정이 탈퇴 되었습니다.");
     }
 
--- src/main/java/com/beancontainer/global/auth/controller/AuthRestController.java
+++ src/main/java/com/beancontainer/global/auth/controller/AuthRestController.java
@@ -4,12 +4,9 @@ import com.beancontainer.domain.member.dto.LoginRequestDTO;
 import com.beancontainer.domain.member.entity.Member;
 import com.beancontainer.domain.member.service.AuthService;
 import com.beancontainer.domain.member.service.MemberService;
-import com.beancontainer.global.auth.jwt.entity.RefreshToken;
 import com.beancontainer.global.auth.jwt.util.JwtTokenizer;
+import com.beancontainer.global.auth.service.CookieService;
 import com.beancontainer.global.auth.service.RefreshTokenService;
-import com.beancontainer.global.exception.CustomException;
-import com.beancontainer.global.exception.ExceptionCode;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -28,10 +25,9 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class AuthRestController {
     private final MemberService memberService;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenizer jwtTokenizer;
     private final RefreshTokenService refreshTokenService;
     private final AuthService authService;
+    private final CookieService cookieService;
 
 
     @PostMapping("/login")
@@ -45,8 +41,8 @@ public class AuthRestController {
         String accessToken = tokens[0];
         String refreshToken = tokens[1];
 
-        addCookie(response, "accessToken", accessToken, (int) (JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
-        addCookie(response, "refreshToken", refreshToken, (int) (JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT / 1000));
+        cookieService.addCookie(response, "accessToken", accessToken, (int) (JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
+        cookieService.addCookie(response, "refreshToken", refreshToken, (int) (JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT / 1000));
 
         Member member = memberService.findByUserId(loginRequestDTO.getUserId());
 
@@ -62,60 +58,30 @@ public class AuthRestController {
     //RefreshToken 을 통한 토큰 재발급
     @PostMapping("/refreshToken")
     public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
-        String refreshToken = getCookieValue(request, "refreshToken");
+        String refreshToken = cookieService.getCookieValue(request, "refreshToken");
         log.info("기존 리프레쉬 토큰 : " + refreshToken);
 
         String[] tokens = refreshTokenService.refreshToken(refreshToken);
         String newAccessToken = tokens[0];
         String newRefreshToken = tokens[1];
 
-        addCookie(response, "accessToken", newAccessToken, (int) (JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
-        addCookie(response, "refreshToken", newRefreshToken, (int) (JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT / 1000));
+        cookieService.addCookie(response, "accessToken", newAccessToken, (int) (JwtTokenizer.ACCESS_TOKEN_EXPIRE_COUNT / 1000));
+        cookieService.addCookie(response, "refreshToken", newRefreshToken, (int) (JwtTokenizer.REFRESH_TOKEN_EXPIRE_COUNT / 1000));
 
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
-        String refreshToken = getCookieValue(request, "refreshToken");
+        String refreshToken = cookieService.getCookieValue(request, "refreshToken");
         authService.logout(refreshToken);
 
-        deleteCookie(response, "accessToken");
-        deleteCookie(response, "refreshToken");
+        cookieService.deleteCookie(response, "accessToken");
+        cookieService.deleteCookie(response, "refreshToken");
 
         return ResponseEntity.ok("로그아웃되었습니다.");
     }
 
-    //쿠키에 토큰 추가
-    private void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge); //유효기간 따로 지정해주기
-        response.addCookie(cookie);
-    }
 
-    //쿠키 삭제
-    private void deleteCookie(HttpServletResponse response, String name) {
-        Cookie cookie = new Cookie(name, null);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-    }
-
-    //쿠키에서 값 추출 함
-    private String getCookieValue(HttpServletRequest request, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (name.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
 }
 
