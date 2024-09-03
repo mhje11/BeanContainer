f88e6061a20039d633ba1043d75e79099c1a5686
minjin00 <dnfenskaao@naver.com>
2024-09-03 20:26:52 +0900

    Fix: Cookie httponly false

--- src/main/java/com/beancontainer/global/auth/service/CookieService.java
+++ src/main/java/com/beancontainer/global/auth/service/CookieService.java
@@ -13,16 +13,11 @@ public class CookieService {
     //쿠키에 토큰 추가
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
-
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
-        cookie.setAttribute("SameSite", "None");
-        cookie.setDomain("http://43.202.33.1:8080/");
-        log.info("addCookie : " + cookie);
         response.addCookie(cookie);
-
-        String setCookieHeader = response.getHeader("Set-Cookie");
-        log.info("Set-Cookie header: {}", setCookieHeader);
     }
 
     //쿠키 삭제
@@ -30,7 +25,6 @@ public class CookieService {
         Cookie cookie = new Cookie(name, null);
         cookie.setPath("/");
         cookie.setMaxAge(0);
-        cookie.setAttribute("SameSite", "None");
         response.addCookie(cookie);
     }
 
