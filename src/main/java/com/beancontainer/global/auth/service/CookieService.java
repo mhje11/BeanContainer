e7204f026572ac91b066be221408c7b0c1e69208
minjin00 <dnfenskaao@naver.com>
2024-09-03 19:24:43 +0900

    Fix: 쿠키 설정 변경

--- src/main/java/com/beancontainer/global/auth/service/CookieService.java
+++ src/main/java/com/beancontainer/global/auth/service/CookieService.java
@@ -13,12 +13,12 @@ public class CookieService {
     //쿠키에 토큰 추가
     public void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
-//        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(maxAge);
-        cookie.setAttribute("SameSite", "None");
-        cookie.setDomain("http://43.202.33.1:8080/");
+//        cookie.setAttribute("SameSite", "None");
+//        cookie.setDomain("http://43.202.33.1:8080/");
         log.info("addCookie : " + cookie);
         response.addCookie(cookie);
 
@@ -32,7 +32,7 @@ public class CookieService {
         cookie.setPath("/");
 //        cookie.setHttpOnly(true);
         cookie.setMaxAge(0);
-        cookie.setAttribute("SameSite", "None");
+//        cookie.setAttribute("SameSite", "None");
         response.addCookie(cookie);
     }
 
